b58314d4eaaee8246998b62a0d87221f306e0738
Luc Kromer <luc.kromer@ffhs.ch>
2025-02-25 10:17:00 +0100

    Add Notification customisation on approval action

---
+++ src/Traits/HasApprovalNotification.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Ffhs\Approvals\Traits;
+
+use Filament\Notifications\Notification;
+
+
+trait HasApprovalNotification
+{
+
+    protected null|string|\Closure|Notification $notificationOnChangeApproval;
+    protected null|string|\Closure|Notification $notificationOnResetApproval;
+    protected null|string|\Closure|Notification $notificationOnSetApproval;
+
+    public function notificationOnChangeApproval(null|string|\Closure|Notification $notificationOnChangeApproval
+    ): static {
+        $this->notificationOnChangeApproval = $notificationOnChangeApproval;
+        return $this;
+    }
+
+    public function notificationOnResetApproval(null|string|\Closure|Notification $notificationOnResetApproval): static
+    {
+        $this->notificationOnResetApproval = $notificationOnResetApproval;
+        return $this;
+    }
+
+    public function notificationOnSetApproval(null|string|\Closure|Notification $notificationOnSetApproval): static
+    {
+        $this->notificationOnSetApproval = $notificationOnSetApproval;
+        return $this;
+    }
+
+
+    public function sendNotificationOnChangeApproval(string $status, string $last): void
+    {
+        $notification = $this->evaluate($this->notificationOnChangeApproval, [
+            'status' => $status,
+            'lastStatus' => $last,
+        ]);
+
+        if ($notification == null) return;
+        if ($notification instanceof Notification) {
+            $notification->send();
+            return;
+        }
+
+        Notification::make()->title($notification)->success()->send();
+    }
+
+    public function sendNotificationOnResetApproval(string $last): void
+    {
+        $notification = $this->evaluate($this->notificationOnResetApproval, [
+            'lastStatus' => $last,
+        ]);
+
+        if ($notification == null) return;
+        if ($notification instanceof Notification) {
+            $notification->send();
+            return;
+        }
+
+        Notification::make()->title($notification)->success()->send();
+    }
+
+    public function sendNotificationOnSetApproval(string $status): void
+    {
+        $notification = $this->evaluate($this->notificationOnSetApproval, [
+            'status' => $status,
+        ]);
+
+        if ($notification == null) return;
+        if ($notification instanceof Notification) {
+            $notification->send();
+            return;
+        }
+
+        Notification::make()->title($notification)->success()->send();
+    }
+
+
+}
