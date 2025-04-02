d1e7250c3c8ee844546f3029baeaf12db4b8a21a
Luc Kromer <luc.kromer@ffhs.ch>
2025-04-02 09:10:48 +0200

    fixing tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace Ffhs\Approvals\Tests;
 use App\Models\User;
 use Database\Seeders\DatabaseSeeder;
 use Filament\Facades\Filament;
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
 use Orchestra\Testbench\Attributes\WithMigration;
@@ -22,9 +23,9 @@ abstract class TestCase extends Orchestra
 
     protected function getEnvironmentSetUp($app)
     {
-//        $loader = AliasLoader::getInstance();
-//        $migration = include __DIR__.'/../workbench/database/migrations/0_create_test_approvable_model.php';
-//        $migration->up();
+        $loader = AliasLoader::getInstance();
+        $migration = include __DIR__ . '/../workbench/database/migrations/0_create_test_approvable_model.php';
+        $migration->up();
     }
 
 
@@ -44,45 +45,3 @@ abstract class TestCase extends Orchestra
         $this->actingAs(User::query()->first());
     }
 }
-
-//class TestCase extends Orchestra
-//{
-//
-//    protected function setUp(): void
-//    {
-//        parent::setUp();
-//
-//        Factory::guessFactoryNamesUsing(
-//            fn (string $modelName) => 'Ffhs\\Approvals\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
-//        );
-//    }
-//
-//    protected function getPackageProviders($app)
-//    {
-//        return [
-//            ActionsServiceProvider::class,
-//            BladeCaptureDirectiveServiceProvider::class,
-//            BladeHeroiconsServiceProvider::class,
-//            BladeIconsServiceProvider::class,
-//            FilamentServiceProvider::class,
-//            FormsServiceProvider::class,
-//            InfolistsServiceProvider::class,
-//            LivewireServiceProvider::class,
-//            NotificationsServiceProvider::class,
-//            SupportServiceProvider::class,
-//            TablesServiceProvider::class,
-//            WidgetsServiceProvider::class,
-//            ApprovalsServiceProvider::class,
-//        ];
-//    }
-//
-//    public function getEnvironmentSetUp($app)
-//    {
-//        config()->set('database.default', 'testing');
-//
-//        /*
-//        $migration = include __DIR__.'/../database/migrations/create_filament-package_ffhs_approvals_table.php.stub';
-//        $migration->up();
-//        */
-//    }
-//}
